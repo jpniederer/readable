e1a2fcb687cda20bd121b9906ff825e9a91d4c76
Joshua Niederer <josh@jpniederer.com>
2017-08-14 21:54:19 -0500

    Setting up folder structure and adding dependencies.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { connect } from 'react-redux';
+import logo from '../logo.svg';
+import '../App.css';
 
 class App extends Component {
   render() {
@@ -11,11 +12,19 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Getting started with Readable.
         </p>
       </div>
     );
   }
 }
 
-export default App;
+function mapStateToProps({}) {
+  return {}
+}
+
+function mapDispatchToProps(dispatch) {
+  return {}
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
